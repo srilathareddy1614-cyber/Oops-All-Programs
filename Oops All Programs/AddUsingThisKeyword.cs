5b67442dd933e633fd313e75f04268a9951c4a0f
srilatha reddy <srilatha reddy@SRILATHAREDDY>
2026-05-21 11:24:39 +0530

    commit

--- Oops All Programs/AddUsingThisKeyword.cs
+++ Oops All Programs/AddUsingThisKeyword.cs
@@ -1,32 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Oops_All_Programs
-{
-    class newclass
-    {
-        int a;
-        int b;
-        public newclass(int a, int b)
-        {
-            this.a = a;
-            this.b = b;
-        }
-        public void Add()
-        {
-            int c= a + b;
-            Console.WriteLine("sum is: " + c);
-        }
-    }
-    internal class AddUsingThisKeyword
-    {
-        static void Main(string[] args)
-        {
-            newclass obj = new newclass(25, 38);
-            obj.Add();
-        }
-    }
-}
+//namespace Oops_All_Programs
+//{
+//    class newclass
+//    {
+//        int a;
+//        int b;
+//        public newclass(int a, int b)
+//        {
+//            this.a = a;
+//            this.b = b;
+//        }
+//        public void Add()
+//        {
+//            int c= a + b;
+//            Console.WriteLine("sum is: " + c);
+//        }
+//    }
+//    internal class AddUsingThisKeyword
+//    {
+//        static void Main(string[] args)
+//        {
+//            newclass obj = new newclass(25, 38);
+//            obj.Add();
+//        }
+//    }
+//}
---
+++ Oops All Programs/Student1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Oops_All_Programs
+{
+    class student
+    {
+        int id;
+        string name;
+        static string clg = "Navodaya";
+    
+    public student(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+        public void Details()
+        {
+            Console.WriteLine("studnt id is: " + id);
+            Console.WriteLine("student name is: " + name);
+            Console.WriteLine("student clg is: " + clg);
+        }
+    }
+    internal class Student1
+    {
+        static void Main(string[] args)
+        {
+            student obj = new student(101, "srilatha");
+            obj.Details();
+        }
+    }
+}
