96eb23c2a5cc1a8e9c5bacc37af99b3d16bbb8cb
srilatha reddy <srilatha reddy@SRILATHAREDDY>
2026-05-21 15:41:14 +0530

    commit

---
+++ Oops All Programs/ConstOl2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+namespace Oops_All_Programs
+{
+    class overload
+    {
+        int id;
+        string name;
+        int sal;
+        public overload()
+        {
+            id = 101;
+            name = "anu";
+            sal = 35000;
+        }
+        public overload(int id,string name, int sal)
+        {
+            this.id = id;
+            this.name = name;
+            this.sal = sal;
+        }
+        public overload(string name)
+        {
+            this.name = name;
+        }
+        public overload(int sal)
+        {
+            this.sal=sal;
+        }
+        //public ConstOl2(int id)
+        //{
+        //    this.id = id;
+        //}
+        public void Display()
+        {
+            Console.WriteLine("emp id:" + id);
+            Console.WriteLine("emp name:" + name);
+            Console.WriteLine("emp sal:" + sal);
+        }
+    }
+    internal class ConstOl2
+    {
+        static void Main(string[] args)
+        {
+            overload ove = new overload();
+            ove.Display();
+            overload obj = new overload(102, "ash", 3456);
+            obj.Display();
+            overload load = new overload("da");
+            load.Display();
+            overload ol = new overload(34567);
+            ol.Display();
+
+        }
+    }
+}
--- Oops All Programs/ConstructorOverloaded.cs
+++ Oops All Programs/ConstructorOverloaded.cs
@@ -1,51 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Oops_All_Programs
-{ 
-    class student
-    {
-        int id;
-        string name;
-        string clg;
-        public student()
-        {
-            id = 101;
-            name = "anu";
-            clg = "xyz";
-        }
-        public student(int id,string name,string clg)
-        {
-            this.id = id;
-            this.name = name;
-            this.clg = clg;
-        }
-        public student(int id,string name)
-        {
-            this.id = id;
-            this.name = name;
-            clg = "not provided";
-        }
-        public void Display()
-        {
-            Console.WriteLine("student id is: " + id);
-            Console.WriteLine("student name is: " + name);
-            Console.WriteLine("student college is: " + clg);
-        }
-    }
-    internal class ConstructorOverloaded
-    {
-        static void Main(string[] args)
-        {
-            student stu=new student();
-            stu.Display();
-            student obj=new student(102,"sri","HIJ");
-            obj.Display();
-            student para = new student(103, "latha");
-            para.Display();
-        }
-    }
-}
+﻿//using System;
+//namespace Oops_All_Programs
+//{ 
+//    class student
+//    {
+//        int id;
+//        string name;
+//        string clg;
+//        public student()
+//        {
+//            id = 101;
+//            name = "anu";
+//            clg = "xyz";
+//        }
+//        public student(int id,string name,string clg)
+//        {
+//            this.id = id;
+//            this.name = name;
+//            this.clg = clg;
+//        }
+//        public student(int id,string name)
+//        {
+//            this.id = id;
+//            this.name = name;
+//            clg = "not provided";
+//        }
+//        public void Display()
+//        {
+//            Console.WriteLine("student id is: " + id);
+//            Console.WriteLine("student name is: " + name);
+//            Console.WriteLine("student college is: " + clg);
+//        }
+//    }
+//    internal class ConstructorOverloaded
+//    {
+//        static void Main(string[] args)
+//        {
+//            student stu=new student();
+//            stu.Display();
+//            student obj=new student(102,"sri","HIJ");
+//            obj.Display();
+//            student para = new student(103, "latha");
+//            para.Display();
+//        }
+//    }
+//}
