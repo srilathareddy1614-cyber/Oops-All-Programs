e4cb5a2bd81668b2286d82fb52411b537914cc8f
srilatha reddy <srilatha reddy@SRILATHAREDDY>
2026-05-23 12:01:55 +0530

    commit

--- Oops All Programs/Public_am.cs
+++ Oops All Programs/Public_am.cs
@@ -1,36 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Oops_All_Programs
-{ 
-    class access
-    {
-        public int a = 10;
-        public void Mymethod()
-        {
-            Console.WriteLine("value of a is : " + a);
-        }
-    }
-    class class1
-    {
-        public void method()
-        {
-            access a=new access();
-            Console.WriteLine("value of a under method is : " + a.a);
-        }
-    }
-    internal class Public_am
-    {
-        static void Main(string[] args)
-        {
-            access a=new access();
-            a.Mymethod();
-            Console.WriteLine("valus of a under main method is :" + a.a);
-            class1 cs = new class1();
-            cs.method();
-        }
-    }
-}
+//namespace Oops_All_Programs
+//{ 
+//    class access
+//    {
+//        public int a = 10;
+//        public void Mymethod()
+//        {
+//            Console.WriteLine("value of a is : " + a);
+//        }
+//    }
+//    class class1
+//    {
+//        public void method()
+//        {
+//            access a=new access();
+//            Console.WriteLine("value of a under method is : " + a.a);
+//        }
+//    }
+//    internal class Public_am
+//    {
+//        static void Main(string[] args)
+//        {
+//            access a=new access();
+//            a.Mymethod();
+//            Console.WriteLine("valus of a under main method is :" + a.a);
+//            class1 cs = new class1();
+//            cs.method();
+//        }
+//    }
+//}
---
+++ Oops All Programs/Singleresponsible.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace Oops_All_Programs
+{
+    class reports
+    {
+        public void Getreports()
+        {
+            Console.WriteLine("reports content");
+        }
+        public void Printreports()
+        {
+            Console.WriteLine("prints printed");
+        }
+        public void Billreports()
+        {
+            Console.WriteLine("bill generated");
+        }
+    }
+    class Getreports
+    {
+        public void Getreportss()
+        {
+            Console.WriteLine("reports content");
+        }
+    }
+    class printreports
+    {
+        public void Billreports()
+        {
+            Console.WriteLine("print reports");
+        }
+        public void Msword()
+        {
+            Console.WriteLine("Ms word documents");
+        }
+        public void Pdfprint()
+        {
+            Console.WriteLine("pdf print");
+        }
+    }
+    class billreports
+    {
+        public void billreoprt()
+        {
+            Console.WriteLine("bill generated");
+        }
+    }
+    internal class Singleresponsible
+    {
+        static void Main(string[] args)
+        {
+            reports r = new reports();
+            r.Getreports();
+            r.Printreports();
+            r.Billreports();
+        }
+    }
+}
