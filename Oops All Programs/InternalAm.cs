3d44217226e862bc7fde227a9dc9a3274e17bd18
srilatha reddy <srilatha reddy@SRILATHAREDDY>
2026-05-25 12:57:54 +0530

    commit

---
+++ Oops All Programs/InternalAm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Oops_All_Programs
+{
+    class myclass
+    {
+        internal int a = 10;
+        public void Mymethod()
+        {
+            Console.WriteLine("value of a under my method is: " + a);
+        }
+    }
+    class newclass
+    {
+        public void Newmethod()
+        {
+            myclass obj = new myclass();
+            Console.WriteLine("value of a under new method is: " + obj.a);
+        }
+    }
+    internal class InternalAm
+    {
+        static void Main(string[] args)
+        {
+            myclass mc= new myclass();
+            mc.Mymethod();
+            Console.WriteLine("value of a under main method is: " + mc.a);
+            newclass nc=new newclass();
+            nc.Newmethod();
+        }
+    }
+}
