3140a016be08f916201fd47ce8c8578087b2c668
srilatha reddy <srilatha reddy@SRILATHAREDDY>
2026-05-26 12:38:53 +0530

    commit

--- Oops All Programs/CopyConstructor2.cs
+++ Oops All Programs/CopyConstructor2.cs
@@ -1,38 +1,38 @@
-﻿using System;
-namespace Oops_All_Programs
-{
-    public class Person
-    {
-        public int Age;
-        public string Name;
-        public Person(string name, int age)
-        {
-            Name = name;
-            Age = age;
-        }
-        public Person(Person previousperson)
-        {
-            Name = previousperson.Name;
-            Age = previousperson.Age;
-        }
-        public string Details()
-        {
-            return Name + " is " + Age.ToString();
-        }
-    }
-    internal class CopyConstructor2
-    {
-        static void Main(string[] args)
-        {
-            Person p1 = new Person("goerge", 39);
-            Person p2 = new Person(p1);
-            p1.Age = 39;
-            p2.Age = 41;
-            p2.Name = "charles";
-            Console.WriteLine(p1.Details());
-            Console.WriteLine(p2.Details());
-            Console.WriteLine(" press any key to exit");
-            Console.ReadKey();
-        }
-    }
-}
+﻿//using System;
+//namespace Oops_All_Programs
+//{
+//    public class Person
+//    {
+//        public int Age;
+//        public string Name;
+//        public Person(string name, int age)
+//        {
+//            Name = name;
+//            Age = age;
+//        }
+//        public Person(Person previousperson)
+//        {
+//            Name = previousperson.Name;
+//            Age = previousperson.Age;
+//        }
+//        public string Details()
+//        {
+//            return Name + " is " + Age.ToString();
+//        }
+//    }
+//    internal class CopyConstructor2
+//    {
+//        static void Main(string[] args)
+//        {
+//            Person p1 = new Person("goerge", 39);
+//            Person p2 = new Person(p1);
+//            p1.Age = 39;
+//            p2.Age = 41;
+//            p2.Name = "charles";
+//            Console.WriteLine(p1.Details());
+//            Console.WriteLine(p2.Details());
+//            Console.WriteLine(" press any key to exit");
+//            Console.ReadKey();
+//        }
+//    }
+//}
---
+++ Oops All Programs/StaticConstructor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_All_Programs
+{ 
+    class student
+    {
+        int id;
+        string name;
+        static string clg;
+        public student(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+        static student()
+        {
+            clg = "JNTU";
+        }
+        public void Display()
+        {
+            Console.WriteLine("student id is : " + id);
+            Console.WriteLine("student name is  : " + name);
+        }
+        public static void CollegeDispaly()
+        {
+            Console.WriteLine("student college is : " + clg);
+        }
+    }
+    internal class StaticConstructor
+    {
+        static void Main(string[] args)
+        {
+            student obj = new student(1, "ABC");
+            obj.Display();
+            student s = new student(2, "XYZ");
+            s.Display();
+            student stu = new student(3,"AYO");
+            stu.Display();
+            student.CollegeDispaly();
+        }
+    }
+}
