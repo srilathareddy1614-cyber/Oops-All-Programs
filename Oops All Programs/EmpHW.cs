62969057eb7893065aa9cc2088d742ba2f8f44a3
srilatha reddy <srilatha reddy@SRILATHAREDDY>
2026-05-26 13:56:13 +0530

    commit

---
+++ Oops All Programs/EmpHW.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_All_Programs
+{
+    class Employee
+    {
+        int id;
+        string name;
+        double sal;
+        static string company;
+        double salhike;
+        public Employee(int id, string name, double sal)
+        {
+            this.id = id;
+            this.name = name;
+            this.sal = sal;
+        }
+        static Employee()
+        {
+            company = "infosys";
+        }
+        public void Display()
+        {
+            Console.WriteLine("employee id is : " + id);
+            Console.WriteLine("employee name is : " + name);
+            Console.WriteLine("employee salary is : " + sal);
+        }
+        public static void companydisplay()
+        {
+            Console.WriteLine("employee company is : " + company);
+        }
+        public void salaryhike( double per)
+        {
+            salhike = sal * per / 100;
+            sal = sal + salhike;
+            Console.WriteLine("hike : " + salhike);
+            Console.WriteLine("salary after hike : " + sal);
+        }
+    }
+    internal class EmpHW
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee(1, "ravi", 30000);
+            emp.Display();
+            Employee.companydisplay();
+            emp.salaryhike(10);
+        }
+    }
+}
