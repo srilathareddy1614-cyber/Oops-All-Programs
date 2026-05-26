f1e7dae98a65a43c7eb88d6ed49fd9fbb35f886a
srilatha reddy <srilatha reddy@SRILATHAREDDY>
2026-05-26 12:27:11 +0530

    commit

--- Oops All Programs/CopyConstructor.cs
+++ Oops All Programs/CopyConstructor.cs
@@ -1,35 +1,35 @@
-﻿using System;
-namespace Oops_All_Programs
-{
-    class Student
-    {
-        public int Id;
-        public string Name;
-        public Student(int id, string name)
-        {
-            Id = id;
-            Name = name;
-        }
-        public Student(Student stu)
-        {
-            Id = stu.Id;
-            Name = stu.Name;
-        }
-        public void Display()
-        {
-            Console.WriteLine("student id is : " + Id);
-            Console.WriteLine("student name is : " + Name);
-        }
-    }
-    internal class CopyConstructor
-    {
-        static void Main(string[] args)
-        {
-            Student obj = new Student(1, "abc");
-            obj.Display();
-            Student stu = new Student(obj);
-            stu.Display();
-        }
+﻿//using System;
+//namespace Oops_All_Programs
+//{
+//    class Student
+//    {
+//        public int Id;
+//        public string Name;
+//        public Student(int id, string name)
+//        {
+//            Id = id;
+//            Name = name;
+//        }
+//        public Student(Student stu)
+//        {
+//            Id = stu.Id;
+//            Name = stu.Name;
+//        }
+//        public void Display()
+//        {
+//            Console.WriteLine("student id is : " + Id);
+//            Console.WriteLine("student name is : " + Name);
+//        }
+//    }
+//    internal class CopyConstructor
+//    {
+//        static void Main(string[] args)
+//        {
+//            Student obj = new Student(1, "abc");
+//            obj.Display();
+//            Student stu = new Student(obj);
+//            stu.Display();
+//        }
 
-    }
-}
+//    }
+//}
