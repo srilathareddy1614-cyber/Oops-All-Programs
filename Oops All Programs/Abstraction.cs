9ab233e42cd19f68fd725616d296de91b1d46d2d
srilatha reddy <srilatha reddy@SRILATHAREDDY>
2026-05-28 11:43:45 +0530

    commit

--- Oops All Programs/Abstraction.cs
+++ Oops All Programs/Abstraction.cs
@@ -1,43 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Oops_All_Programs
-{
-    abstract class project
-    {
-        public void Start()
-        {
-            Console.WriteLine("started the class");
-        }
-        public void Resume()
-        {
-            Console.WriteLine("resume the class");
-        }
-        public void Display()
-        {
-            Console.WriteLine("welcome");
-        }
-        public abstract void End();
-    }
-    class DerivedClass:project
-    {
-        public override void End()
-        {
-            Console.WriteLine("session has ended");
-        }
-    }
-    internal class Abstraction
-    {
-        static void Main(string[] args)
-        {
-            project p = new DerivedClass();
-            p.Display();
-            p.Start();
-            p.Resume();
-            p.End();
-        }
-    }
-}
+//namespace Oops_All_Programs
+//{
+//    abstract class project
+//    {
+//        public void Start()
+//        {
+//            Console.WriteLine("started the class");
+//        }
+//        public void Resume()
+//        {
+//            Console.WriteLine("resume the class");
+//        }
+//        public void Display()
+//        {
+//            Console.WriteLine("welcome");
+//        }
+//        public abstract void End();
+//    }
+//    class DerivedClass:project
+//    {
+//        public override void End()
+//        {
+//            Console.WriteLine("session has ended");
+//        }
+//    }
+//    internal class Abstraction
+//    {
+//        static void Main(string[] args)
+//        {
+//            project p = new DerivedClass();
+//            p.Display();
+//            p.Start();
+//            p.Resume();
+//            p.End();
+//        }
+//    }
+//}
---
+++ Oops All Programs/VirtualMethod.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_All_Programs
+{
+    class myclass
+    {
+        public void Display()
+        {
+            Console.WriteLine("welcome to oops");
+        }
+        public virtual void show()
+        {
+            Console.WriteLine("show is calling");
+        }
+    }
+    class dc : myclass
+    {
+        public void Message()
+        {
+            Console.WriteLine("message is calling");
+        }
+        public override void show()
+        {
+            //base.show();
+            Console.WriteLine("this is overriden method");
+        }
+    }
+    internal class VirtualMethod
+    {
+        static void Main(string[] args)
+        {
+            myclass obj = new myclass();
+            obj.Display();
+            obj.show();
+            dc objdc = new dc();
+            objdc.Message();
+            objdc.show();
+        }
+    }
+}
